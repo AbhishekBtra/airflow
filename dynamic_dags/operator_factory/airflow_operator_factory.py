0f33f18a708aaa00d037995212bcc719f1ce554b
Abhishek Batra <abatra002@gmail.com>
2025-04-24 17:42:22 +0530

    add bq insert job operator

--- dynamic_dags/operator_factory/airflow_operator_factory.py
+++ dynamic_dags/operator_factory/airflow_operator_factory.py
@@ -4,6 +4,7 @@ import sys
 
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
@@ -20,6 +21,15 @@ class AirflowOperatorFactory():
     @staticmethod
     def get_python_operator(dag, task_id, args):
         task = PythonOperator(
+            dag = dag,
+            task_id = task_id,
+            **args
+        )
+        return task
+    
+    @staticmethod
+    def get_bq_insert_job_operator(dag, task_id, args):
+        task = BigQueryInsertJobOperator(
             dag = dag,
             task_id = task_id,
             **args
